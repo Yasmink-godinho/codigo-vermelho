5e6fac95863086728ff1721a41b2c5ce11dae01a
Yasmin Godinho <yasmingodinhosilvaa@gmail.com>
2026-08-31 20:11:44 -0300

    ajuste estrutura projeto

---
+++ src/main/java/com/codigovermelho/CodigoVermelhoApplication.java
@@ -0,0 +1,13 @@
+package com.codigovermelho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodigoVermelhoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodigoVermelhoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codigovermelho/CodigoVermelhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codigovermelho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodigoVermelhoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
