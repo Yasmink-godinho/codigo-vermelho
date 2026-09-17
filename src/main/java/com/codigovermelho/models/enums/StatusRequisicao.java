fb2237ede52a2aa0360d66f616df203f223a0562
Yasmin Godinho <yasmingodinhosilvaa@gmail.com>
2026-09-16 22:48:53 -0300

    feat(us04): implementa estrutura backend para emissao de requisicoes

--- src/main/java/com/codigovermelho/models/enums/StatusRequisicao.java
+++ src/main/java/com/codigovermelho/models/enums/StatusRequisicao.java
@@ -1,4 +1,9 @@
 package com.codigovermelho.models.enums;
 
 public enum StatusRequisicao {
+    PENDENTE,
+    EM_ANALISE,
+    APROVADA,
+    RECUSADA,
+    ATENDIDA
 }
--- src/main/java/com/codigovermelho/repositories/RequisicaoRepository.java
+++ src/main/java/com/codigovermelho/repositories/RequisicaoRepository.java
@@ -1,4 +1,15 @@
 package com.codigovermelho.repositories;
 
-public interface RequisicaoRepository {
+import com.codigovermelho.models.Requisicao;
+import com.codigovermelho.models.enums.NivelUrgencia;
+import com.codigovermelho.models.enums.StatusRequisicao;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface RequisicaoRepository extends JpaRepository<Requisicao, Long> {
+
+    List<Requisicao> findByStatus(StatusRequisicao status);
+
+    List<Requisicao> findByNivelUrgencia(NivelUrgencia nivelUrgencia);
 }
